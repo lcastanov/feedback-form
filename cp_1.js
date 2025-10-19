40fa85bfe1ef7213133b89fec95bcfb1aa8c542e
Laura <lauracastanovilla@usf.edu>
2025-10-19 18:43:09 -0400

    starting out submission prevention

--- cp_1.js
+++ cp_1.js
@@ -11,3 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 })
 
+
+
+// Preventing submission if field is empty
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('feedback-form');
+    const userInput = document.getElementById('user-name');
+    const emailInput = document.getElementById('email');
+    const commentsInput = document.getElementById('comments');
+
+    const userError = document.getElementById('username-error');
+    const emailError = document.getElementById('email-error');
+    const commentsError = document.getElementById('comments-error');
+})
